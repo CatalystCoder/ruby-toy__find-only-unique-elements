aed6ef468d4f25c4f1a84d16d5b31cbd1ec4f1c4
Tyler Fisher <tfisher322@gmail.com>
2016-01-20 23:33:12 +0000

    Unique

--- lib/unique_elements.rb
+++ lib/unique_elements.rb
@@ -1,6 +1,13 @@
 # This method takes an array of elements (these might be strings, integers, floats, or a
 # combination of the above), and returns an array of the elements that appear once and only
 # once.
-def find_unique_elements(arr)
-  
+def find_unique_elements(arr)   
+    counts = {}
+arr.each do |num|
+  count = arr.find_all { |x| x == num }.size
+  counts[num] = count if count <= 1
+end
+
+elements = arr.find_all { |x| counts.has_key?(x) }
+p elements
 end
